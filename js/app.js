6188b031f6087f051619cb37b4d88e6d1e145761
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-02-17 16:29:32 +0200

    added app.js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+ // JavaScript functionality to implement
+ const playButtons = document.querySelectorAll('.play-button');
+ playButtons.forEach(button => {
+   button.addEventListener('click', () => {
+     // Code to play the song
+   });
+ });
+
+ const downloadButtons = document.querySelectorAll('.download-button');
+ downloadButtons.forEach(button => {
+   button.addEventListener('click', () => {
+     // Code to download the song
+   });
+ });
+
+ const likeButtons = document.querySelectorAll('.like-button');
+ likeButtons.forEach(button => {
+   button.addEventListener('click', () => {
+     // Code to like the song and store the like using JavaScript
+   });
+ });
