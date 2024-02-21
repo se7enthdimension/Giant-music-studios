53fb3cc025e81d956c3d5cd03e64cf12116e9643
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-02-21 02:02:42 +0200

    progress to sleep

--- js/app.js
+++ js/app.js
@@ -232,3 +232,43 @@ volumeRange.addEventListener("input",setVolume);
 repeatBtn.addEventListener("click", repeat);
 likeBtn.addEventListener("click", like);
 songListBtn.addEventListener("click",musicList);
+
+//search
+
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+//stop current audio
+
+const audioElements = document.querySelectorAll('.audio');
+
+audioElements.forEach(audio => {
+    audio.addEventListener('play', function() {
+        pauseAllExcept(audio);
+    });
+});
+
+function pauseAllExcept(currentAudio) {
+    audioElements.forEach(audio => {
+        if (audio !== currentAudio) {
+            audio.pause();
+        }
+    });
+}
